f70596edc295a508ebecc0e027e374ea0c8bd910
SVETO <svetostoj@hotmail.com>
2019-02-26 01:02:22 +1100

    extract dedunduncy and prepare for production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController<ApplicationController
+
+    before_action :set_article, only: [:edit, :update, :show, :distroy]
+    
     def new
         @article = Article.new
     
@@ -43,6 +46,10 @@ class ArticlesController<ApplicationController
     end
 
     private
+    def set_article
+        @article = Article.find(params[:id])
+    end
+
     def article_params
         params.require(:article).permit(:title, :description)
     end
