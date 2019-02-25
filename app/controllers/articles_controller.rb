58be33d123102593521b6dc7a21cc5015a886a49
SVETO <svetostoj@hotmail.com>
2019-02-25 20:37:15 +1100

    Added show edit new and index Article views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,11 +3,37 @@ class ArticlesController<ApplicationController
         @article = Article.new
     
     end
+
+    def index
+        @articles = Article.all 
+    end
+    
+    def show
+        @article = Article.find(params[:id])
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice]="Article was successfully careated"
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+        end
+    end
+
     def create
-        #render plain: params[:article].inspect
-        @article = Article.new(article_params)
-        @article.save
-        redirect_to articles_show(@article)
+        @article=Article.new(article_params)
+        if @article.save
+            flash[:notice]="Article was successfully created"
+            redirect_to article_path(@article)
+        else
+            render 'new'
+        end
     end
 
     private
